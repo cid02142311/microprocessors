167320a47dedd28343ced5c7521151ac21dbac74
cz2022 <cz2022@skbl-418-002.ph.ic.ac.uk>
2024-11-01 12:10:13 +0000

    FEAT: counting loop

--- main.s
+++ main.s
@@ -1,7 +1,7 @@
 	#include <xc.inc>
 
 psect	code, abs
-	
+
 main:
 	org	0x0
 	goto	start
@@ -10,15 +10,62 @@ main:
 start:
 	movlw 	0x0
 	movwf	TRISC, A	    ; Port C all outputs
+	movlw	0x00
+	movwf	0x06, A
+	call	check
 	bra 	test
+check:
+	movlw	0x00
+	btfsc	PORTD, 0
+	addlw	0x01
+	btfsc	PORTD, 1
+	addlw	0x02
+	btfsc	PORTD, 2
+	addlw	0x04
+	btfsc	PORTD, 3
+	addlw	0x08
+	btfsc	PORTD, 4
+	addlw	0x10
+	btfsc	PORTD, 5
+	addlw	0x20
+	btfsc	PORTD, 6
+	addlw	0x40
+	btfsc	PORTD, 7
+	addlw	0x80
+	movwf	0x00
+	return
+delay:
+	movlw	0xff
+	movwf	0x21, A
+	call	delay1
+	decfsz	0x20, A
+	bra	delay
+	return
+delay1:
+	movlw	0x0f
+	movwf	0x22, A
+	call	delay2
+	decfsz	0x21, A
+	bra	delay1
+	return
+delay2:
+	decfsz	0x22, A
+	bra	delay2
+	return
+stop:
+	goto	stop
 loop:
 	movff 	0x06, PORTC
-	incf 	0x06, W, A
+	incf 	0x06, A
+	movlw	0xff
+	movwf	0x20, A
+	call	delay
 test:
-	movwf	0x06, A	    ; Test for end of loop condition
-	movlw 	0x63
+;	movlw	0x63
+	movf 	0x00, W
 	cpfsgt 	0x06, A
 	bra 	loop		    ; Not yet finished goto start of loop again
-	goto 	0x0		    ; Re-run program from start
-
+;	goto 	0x0		    ; Re-run program from start
+	goto	stop
 	end	main
+
