6d52bb5f17d6fe45ac45fde811ad82223deb751a
cz2022 <cz2022@skbl-418-002.ph.ic.ac.uk>
2024-11-07 11:32:44 +0000

    CHORE: master adjustment

--- main.s
+++ main.s
@@ -8,44 +8,63 @@ main:
 
 	org	0x100		    ; Main code starts here at address 0x100
 start:
-	clrf	TRISE, A
-	clrf	PORTE, A
-	lfsr	0, 0x10
+	movlw 	0x0
+	movwf	TRISC, A	    ; Port C all outputs
 	movlw	0x00
-	movwf	INDF0, A
-increment_phase:
-	movf	INDF0, W
-	movwf	PORTE, A
-	incf	INDF0, f
-	call	delay
-	movlw	0xfe		    ; Do not use 0xff: overflow
-	cpfsgt	INDF0, A
-	bra increment_phase
-decrement_phase:
-	movf	INDF0, W
-	movwf	PORTE, A
-	decf	INDF0, f
-	call	delay
+	movwf	0x06, A
+	call	check
+	bra 	test
+check:
 	movlw	0x00
-	cpfseq	INDF0, A
-	bra decrement_phase
-	bra increment_phase 
+	btfsc	PORTD, 0
+	addlw	0x01
+	btfsc	PORTD, 1
+	addlw	0x02
+	btfsc	PORTD, 2
+	addlw	0x04
+	btfsc	PORTD, 3
+	addlw	0x08
+	btfsc	PORTD, 4
+	addlw	0x10
+	btfsc	PORTD, 5
+	addlw	0x20
+	btfsc	PORTD, 6
+	addlw	0x40
+	btfsc	PORTD, 7
+	addlw	0x80
+	movwf	0x00
+	return
 delay:
 	movlw	0xff
 	movwf	0x21, A
-;	call	delay1
+	call	delay1
 	decfsz	0x20, A
 	bra	delay
 	return
 delay1:
-	movlw	0xff
+	movlw	0x0f
 	movwf	0x22, A
 	call	delay2
 	decfsz	0x21, A
-;	bra	delay1
+	bra	delay1
 	return
 delay2:
 	decfsz	0x22, A
 	bra	delay2
 	return
+stop:
+	goto	stop
+loop:
+	movff 	0x06, PORTC
+	incf 	0x06, A
+	movlw	0xff
+	movwf	0x20, A
+	call	delay
+test:
+;	movlw	0x63
+	movf 	0x00, W
+	cpfsgt 	0x06, A
+	bra 	loop		    ; Not yet finished goto start of loop again
+;	goto 	0x0		    ; Re-run program from start
+	goto	stop
 	end	main
