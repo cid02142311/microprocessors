1ed832054c1f297bd9af5dfffebac78d82143b3b
cz2022 <cz2022@skbl-418-002.ph.ic.ac.uk>
2024-11-19 11:41:43 +0000

    FEAT: KeyPad final

--- KeyPad.s
+++ KeyPad.s
@@ -1,6 +1,6 @@
 #include <xc.inc>
     
-global  KeyPad_Setup
+global  KeyPad_Setup, KeyPad_Read
 global	KeyPad_output
 
 psect	udata_acs   ; reserve data space in access ram
@@ -16,56 +16,61 @@ delay:
     bra	    delay
     return
 
+
 KeyPad_Setup:
-    clrf    KeyPad_output
-    clrf    KeyPad_row
-    clrf    KeyPad_column
-    clrf    KeyPad_counter
-;    movlb   15
     banksel PADCFG1
     bsf	    REPU
     clrf    LATE, A
+    return
+    
+    
+KeyPad_Setup_col:
     movlw   00001111B
     movwf   TRISE, A
     movlw   0xff
-    movwf   delay_count
+    movwf   delay_count, A
     call    delay
-    goto    KeyPad_read
-
+    return
+    
 KeyPad_Setup_row:
-    clrf    KeyPad_counter
+    clrf    KeyPad_counter, A
 
     movlw   11110000B
     movwf   TRISE, A
     movlw   0xff
-    movwf   delay_count
+    movwf   delay_count, A
     call    delay
     return
 
-KeyPad_read:
+KeyPad_Read:
+    clrf    KeyPad_output, A
+    clrf    KeyPad_row, A
+    clrf    KeyPad_column, A
+    clrf    KeyPad_counter, A
+    call    KeyPad_Setup_col
     movlw   0x1
     btfss   PORTE, 0, A
-    movwf   KeyPad_column
+    movwf   KeyPad_column, A
     btfss   PORTE, 0, A
-    incf    KeyPad_counter
+    incf    KeyPad_counter, A
 
     movlw   0x2
     btfss   PORTE, 1, A
-    movwf   KeyPad_column
+    movwf   KeyPad_column, A
     btfss   PORTE, 1, A
-    incf    KeyPad_counter
+    incf    KeyPad_counter, A
 
     movlw   0x3
     btfss   PORTE, 2, A
-    movwf   KeyPad_column
+    movwf   KeyPad_column, A
     btfss   PORTE, 2, A
-    incf    KeyPad_counter
+    incf    KeyPad_counter, A
 
     movlw   0x4
     btfss   PORTE, 3, A
-    movwf   KeyPad_column
+    movwf   KeyPad_column, A
     btfss   PORTE, 3, A
-    incf    KeyPad_counter
+    incf    KeyPad_counter, A
 
     ; write errror message from counter
 
@@ -73,27 +78,27 @@ KeyPad_read:
 
     movlw   0x1
     btfss   PORTE, 4, A
-    movwf   KeyPad_row
+    movwf   KeyPad_row, A
     btfss   PORTE, 4, A
-    incf    KeyPad_counter
+    incf    KeyPad_counter, A
 
     movlw   0x2
     btfss   PORTE, 5, A
-    movwf   KeyPad_row
+    movwf   KeyPad_row, A
     btfss   PORTE, 5, A
-    incf    KeyPad_counter
+    incf    KeyPad_counter, A
 
     movlw   0x3
     btfss   PORTE, 6, A
-    movwf   KeyPad_row
+    movwf   KeyPad_row, A
     btfss   PORTE, 6, A
-    incf    KeyPad_counter
+    incf    KeyPad_counter, A
 
     movlw   0x4
     btfss   PORTE, 7, A
-    movwf   KeyPad_row
+    movwf   KeyPad_row, A
     btfss   PORTE, 7, A
-    incf    KeyPad_counter
+    incf    KeyPad_counter, A
 
     ; write errror message from counter
     
@@ -103,29 +108,29 @@ KeyPad_Analysis:
 Check_Row1:
     ; If Row 1 (RE4 is active)
     movlw   0x1
-    cpfseq  KeyPad_row
+    cpfseq  KeyPad_row, A
     bra     Check_Row2
     bra    ret1
 
     ; Now determine the key in Row 1 based on column
 ret1:
     movlw   0x1            ; Column 1 (RE0)
-    cpfseq  KeyPad_column
+    cpfseq  KeyPad_column, A
     bra	    ret2
     retlw   '1'             ; Key '1'
 ret2:
     movlw   0x2            ; Column 2 (RE1)
-    cpfseq  KeyPad_column
+    cpfseq  KeyPad_column, A
     bra	    ret3
     retlw   '2'             ; Key '2'
 ret3:
     movlw   0x3            ; Column 3 (RE2)
-    cpfseq  KeyPad_column
+    cpfseq  KeyPad_column, A
     bra	    retf
     retlw   '3'             ; Key '3'
 retf:
     movlw   0x4            ; Column 4 (RE3)
-    cpfseq  KeyPad_column
+    cpfseq  KeyPad_column, A
     bra     No_KeyDetected
     retlw   'F'             ; Key 'F'
 
@@ -133,29 +138,29 @@ retf:
 Check_Row2:
     ; If Row 2 (RE5 is active)
     movlw   0x2
-    cpfseq  KeyPad_row
+    cpfseq  KeyPad_row, A
     bra     Check_Row3
     bra	    ret4
 
     ; Now determine the key in Row 2 based on column
 ret4:
     movlw   0x1            ; Column 1 (RE0)
-    cpfseq  KeyPad_column
+    cpfseq  KeyPad_column, A
     bra	    ret5
     retlw   '4'             ; Key '4'
 ret5:
     movlw   0x2            ; Column 2 (RE1)
-    cpfseq  KeyPad_column
+    cpfseq  KeyPad_column, A
     bra	    ret6
     retlw   '5'             ; Key '5'
 ret6:
     movlw   0x3            ; Column 3 (RE2)
-    cpfseq  KeyPad_column
+    cpfseq  KeyPad_column, A
     bra	    rete
     retlw   '6'             ; Key '6'
 rete:
     movlw   0x4            ; Column 4 (RE3)
-    cpfseq  KeyPad_column
+    cpfseq  KeyPad_column, A
     bra	    No_KeyDetected
     retlw   'E'             ; Key 'E'
 
@@ -163,29 +168,29 @@ rete:
 Check_Row3:
     ; If Row 3 (RE6 is active)
     movlw   0x3
-    cpfseq  KeyPad_row
+    cpfseq  KeyPad_row, A
     bra     Check_Row4
     bra	    ret7
 
     ; Now determine the key in Row 3 based on column
 ret7:
     movlw   0x1            ; Column 1 (RE0)
-    cpfseq  KeyPad_column
+    cpfseq  KeyPad_column, A
     bra	    ret8
     retlw   '7'             ; Key '7'
 ret8:
     movlw   0x2            ; Column 2 (RE1)
-    cpfseq  KeyPad_column
+    cpfseq  KeyPad_column, A
     bra	    ret9
     retlw   '8'             ; Key '8'
 ret9:
     movlw   0x3            ; Column 3 (RE2)
-    cpfseq  KeyPad_column
+    cpfseq  KeyPad_column, A
     bra	    retd
     retlw   '9'             ; Key '9'
 retd:
     movlw   0x4            ; Column 4 (RE3)
-    cpfseq  KeyPad_column
+    cpfseq  KeyPad_column, A
     bra	    No_KeyDetected
     retlw   'D'             ; Key 'D'
 
@@ -193,33 +198,33 @@ retd:
 Check_Row4:
     ; If Row 4 (RE7 is active)
     movlw   0x4
-    cpfseq  KeyPad_row
+    cpfseq  KeyPad_row, A
     bra     No_KeyDetected
     bra	    reta
 
     ; Now determine the key in Row 4 based on column
 reta:
     movlw   0x1            ; Column 1 (RE0)
-    cpfseq  KeyPad_column
+    cpfseq  KeyPad_column, A
     bra	    ret0
     retlw   'A'             ; Key 'A'
 ret0:
     movlw   0x2            ; Column 2 (RE1)
-    cpfseq  KeyPad_column
+    cpfseq  KeyPad_column, A
     bra	    retb
     retlw   '0'             ; Key '0'
 retb:
     movlw   0x3            ; Column 3 (RE2)
-    cpfseq  KeyPad_column
+    cpfseq  KeyPad_column, A
     bra	    retc
     retlw   'B'             ; Key 'B'
 retc:
     movlw   0x4            ; Column 4 (RE3)
-    cpfseq  KeyPad_column
+    cpfseq  KeyPad_column, A
     bra	    No_KeyDetected
     retlw   'C'             ; Key 'C'
 
 
 No_KeyDetected:
-    clrf    KeyPad_output   ; No key detected, set KeyPad_output to 0
-    goto    KeyPad_Setup
+    clrf    KeyPad_output, A   ; No key detected, set KeyPad_output to 0
+    goto    KeyPad_Read
--- main.s
+++ main.s
@@ -2,23 +2,26 @@
 
 extrn	UART_Setup, UART_Transmit_Message  ; external subroutines
 extrn	LCD_Setup, LCD_Write_Message
-extrn	KeyPad_Setup
+extrn	KeyPad_Setup, KeyPad_Read
 extrn	KeyPad_output
 
 psect	udata_acs   ; reserve data space in access ram
-counter:    ds 1    ; reserve one byte for a counter variable
-delay_count:ds 1    ; reserve one byte for counter in the delay routine
+counter:	ds  1    ; reserve one byte for a counter variable
+delay_count:	ds  1    ; reserve one byte for counter in the delay routine
+delay_count_2:	ds  1
+delay_count_3:	ds  1
+myChar:		ds  1
     
 psect	udata_bank4 ; reserve data anywhere in RAM (here at 0x400)
 myArray:    ds 0x80 ; reserve 128 bytes for message data
 
-psect	data    
-	; ******* myTable, data in programme memory, and its length *****
-myTable:
-	db	'N','o','n','e',0x0a
-					; message, plus carriage return
-	myTable_l   EQU	5	; length of data
-	align	2
+; psect	data    
+; ******* myTable, data in programme memory, and its length *****
+;myTable:
+;	db	'N','o','n','e',0x0a
+;					; message, plus carriage return
+;	myTable_l   EQU	5	; length of data
+;	align	2
 
 psect	code, abs
 main:
@@ -32,43 +35,80 @@ setup:
     bsf	    EEPGD 	; access Flash program memory
     call    UART_Setup	; setup UART
     call    LCD_Setup	; setup UART
+    call    KeyPad_Setup
     goto    start
 
 ; ******* Main programme ****************************************
 start: 	
-    call    KeyPad_Setup
+    call    KeyPad_Read
+    movwf   myChar, A
     
-    lfsr    0, myArray	; Load FSR0 with address in RAM	
-    movlw   low highword(myTable)	; address of data in PM
-    movwf   TBLPTRU, A		; load upper bits to TBLPTRU
-    movlw   high(myTable)	; address of data in PM
-    movwf   TBLPTRH, A		; load high byte to TBLPTRH
-    movlw   low(myTable)	; address of data in PM
-    movwf   TBLPTRL, A		; load low byte to TBLPTRL
-    movlw   myTable_l	; bytes to read
-    movwf   counter, A		; our counter register
+    lfsr    0, myArray	; Load FSR0 with address in RAM
+    movf    myChar, W, A
+    movwf   POSTINC0, A
+    movlw   0x0a
+    movwf   POSTINC0, A
 
-loop: 	
-    tblrd*+			; one byte from PM to TABLAT, increment TBLPRT
-    movff   TABLAT, POSTINC0; move data from TABLAT to (FSR0), inc FSR0	
-    decfsz  counter, A		; count down to zero
-    bra	loop		; keep going until finished
-		
-    movlw   myTable_l	; output message to UART
+    movlw   2
     lfsr    2, myArray
     call    UART_Transmit_Message
 
-    movlw   myTable_l	; output message to LCD
-    addlw   0xff		; don't send the final carriage return to LCD
+    movlw   1
     lfsr    2, myArray
     call    LCD_Write_Message
-;    clrf    LATB, A
-    goto    $		; goto current line in code
+
+    movlw   0xff
+    movwf   delay_count, A
+    call    delay
+
+    goto    start
+
+;    movlw   low highword(myTable)	; address of data in PM
+;    movwf   TBLPTRU, A		; load upper bits to TBLPTRU
+;    movlw   high(myTable)	; address of data in PM
+;    movwf   TBLPTRH, A		; load high byte to TBLPTRH
+;    movlw   low(myTable)	; address of data in PM
+;    movwf   TBLPTRL, A		; load low byte to TBLPTRL
+;    movlw   myTable_l	; bytes to read
+;    movwf   counter, A		; our counter register
+
+;loop: 	
+;    tblrd*+			; one byte from PM to TABLAT, increment TBLPRT
+;    movff   TABLAT, POSTINC0; move data from TABLAT to (FSR0), inc FSR0	
+;    decfsz  counter, A		; count down to zero
+;    bra	loop		; keep going until finished
+;		
+;    movlw   myTable_l	; output message to UART
+;    lfsr    2, myArray
+;    call    UART_Transmit_Message
+;
+;    movlw   myTable_l	; output message to LCD
+;    addlw   0xff		; don't send the final carriage return to LCD
+;    lfsr    2, myArray
+;    call    LCD_Write_Message
+;;    clrf    LATB, A
+;    goto    $		; goto current line in code
 
 ; a delay subroutine if you need one, times around loop in delay_count
 delay:	
+    movlw   0xff
+    movwf   delay_count_2, A
+    call    delay2
     decfsz  delay_count, A	; decrement until zero
-    bra	delay
+    bra	    delay
+    return
+
+delay2:	
+    movlw   0x08
+    movwf   delay_count_3, A
+    call    delay3
+    decfsz  delay_count_2, A	; decrement until zero
+    bra	    delay2
+    return
+
+delay3:
+    decfsz  delay_count_3, A
+    bra	    delay3
     return
 
 end main
