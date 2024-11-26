f51cf37c173583682abf634525df82ed9a657c64
cz2022 <cz2022@skbl-418-001.ph.ic.ac.uk>
2024-11-26 09:58:12 +0000

    CHORE: UART upate

--- UART.s
+++ UART.s
@@ -1,12 +1,12 @@
-#include xc.inc
+#include <xc.inc>
     
 global  UART_Setup, UART_Transmit_Message
 
 psect	udata_acs   ; reserve data space in access ram
-UART_counter ds    1	    ; reserve 1 byte for variable UART_counter
+UART_counter: ds    1	    ; reserve 1 byte for variable UART_counter
 
 psect	uart_code,class=CODE
-UART_Setup
+UART_Setup:
     bsf	    SPEN	; enable
     bcf	    SYNC	; synchronous
     bcf	    BRGH	; slow speed
@@ -18,16 +18,16 @@ UART_Setup
 					; must set TRISC6 to 1
     return
 
-UART_Transmit_Message	    ; Message stored at FSR2, length stored in W
+UART_Transmit_Message:	    ; Message stored at FSR2, length stored in W
     movwf   UART_counter, A
-UART_Loop_message
+UART_Loop_message:
     movf    POSTINC2, W, A
     call    UART_Transmit_Byte
     decfsz  UART_counter, A
     bra	    UART_Loop_message
     return
 
-UART_Transmit_Byte	    ; Transmits byte stored in W
+UART_Transmit_Byte:	    ; Transmits byte stored in W
     btfss   TX1IF	    ; TX1IF is set when TXREG1 is empty
     bra	    UART_Transmit_Byte
     movwf   TXREG1, A
